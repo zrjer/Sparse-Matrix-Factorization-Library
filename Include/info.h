f98d0dda230de5dce0d30469245c34af3c4278c7
蓬莱 <penglai-hourai@outlook.com>
2019-01-21 03:33:54 -0500

    misc

--- Include/info.h
+++ Include/info.h
@@ -4,13 +4,25 @@
 typedef
 struct common_info_struct
 {
+    void *mem;
+    size_t memsize;
 }
 common_info;
 
 typedef
 struct gpu_info_struct
 {
+    void *host_mem;
+    size_t host_memsize;
+    void *dev_mem;
+    size_t dev_memsize;
 }
 gpu_info;
+
+typedef
+struct matrix_info_struct
+{
+}
+matrix_info;
 
 #endif
--- Include/parameters.h
+++ Include/parameters.h
@@ -1,4 +1,15 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
+#define MEM_LIMITS
+
+#ifdef MEM_LIMITS
+    #define MAX_MEM 4
+    #define MAX_HOST_MEM 4
+    #define MAX_DEV_MEM 4
+#else
+#endif
+
+#undef MEM_LIMITS
+
 #endif
